3a48c76de106122b8256efb80496742fb50a7ad1
waqas60 <waqas.official2005@gmail.com>
2025-12-11 20:59:36 +0500

    init: intialize nextjs project and feat(navbar): create the navbar component

---
+++ components/Navbar.tsx
@@ -0,0 +1,41 @@
+"use client";
+import { Moon } from "lucide-react";
+import Link from "next/link";
+import { useState } from "react";
+
+const items = [
+  { href: "/", title: "Home" },
+  { href: "/projects", title: "Projects" },
+  { href: "/blog", title: "Blog" },
+];
+
+export default function Navbar() {
+  const [active, setActive] = useState("Home");
+
+  return (
+    <div className="flex h-30 items-center justify-end px-6 py-2">
+      <div>
+        <ul className="flex items-center justify-center gap-7 rounded-4xl px-6 py-3 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] text-sm">
+          {items.map((item, index) => {
+            return (
+              <li
+                onClick={() => setActive(item.title)}
+                className={`cursor-pointer transition-all duration-300 ease-in-out ${
+                  active === item.title
+                    ? "border-b border-zinc-500"
+                    : "border-transparent text-zinc-500"
+                }`}
+                key={index}
+              >
+                <Link href={item.href}>{item.title}</Link>
+              </li>
+            );
+          })}
+          <li className="cursor-pointer">
+            <Moon />
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
